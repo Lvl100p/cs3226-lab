f2e1c2e67eb2ee300844b1bdbbd8f9e5d679064f
Kai Lin <kailin.t@gmail.com>
2017-02-17 13:41:25 +0800

    Change radar chart on detail view to use Chart.js

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,70 +5,72 @@
 @section('content-title', 'CS3226 Lab: Student Details')
 
 @section('stylesheet')
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap.datatables/0.1/css/datatables.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap.datatables/0.1/css/datatables.css">
 @endsection
 
 @section('script')
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/bootstrap.datatables/0.1/js/datatables.js"></script>
-    <script type="text/javascript" src="https://code.createjs.com/easeljs-0.8.2.min.js"></script>
-    <script type="text/javascript" src="{{ url(asset('js/radarChart.js')) }}"></script>
-    <script type="text/javascript">
-        jQuery(document).ready(function () {
-            jQuery('#ranktable').DataTable();
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/bootstrap.datatables/0.1/js/datatables.js"></script>
+<script type="text/javascript" src="https://code.createjs.com/easeljs-0.8.2.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
+<script type="text/javascript" src="{{ url(asset('js/radarChart.js')) }}"></script>
 
-            radarChart({{$student->mc}}, {{$student->tc}}, {{$student->hw}}, {{$student->bs}}, {{$student->ks}}, {{$student->ac}});
+<script type="text/javascript">
+    jQuery(document).ready(function () {
+        jQuery('#ranktable').DataTable();
 
-        });
+        radarChart({{$student->mc}}, {{$student->tc}}, {{$student->hw}}, {{$student->bs}}, {{$student->ks}}, {{$student->ac}});
 
-        jQuery('#form-delete').on('submit', function(){
-            return confirm("Are you sure you want to delete?");
-        });
-    </script>
+    });
+
+    jQuery('#form-delete').on('submit', function(){
+        return confirm("Are you sure you want to delete?");
+    });
+</script>
 @endsection
 
 @section('content')
-    <section id="summary">
-        <h1>{{$student->name}}
-            <small>in CS3233 S2 AY 2016/17</small>
-        </h1>
+<section id="summary">
+    <h1>{{$student->name}}
+        <small>in CS3233 S2 AY 2016/17</small>
+    </h1>
 
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6"><canvas id="radarChart" width="550", height="550"></canvas></div>
-                <div class="col-md-6 vcenter" style="height: 500px;">
-                    <table class="table">
-                        <tr>
-                            <td><img src="/img/uploads/{{$student->id}}.png" width="200" height="200"/> </td>
-                        </tr>
-                        <tr>
-                            <td>Kattis account</td>
-                            <td>{{$student->nickname}}</td>
-                        </tr>
-                        <tr>
-                            <td>SPE(ed) component</td>
-                            <td>{{$student->mc}} + {{$student->tc}} = {{$student->spe}}</td>
-                        </tr>
-                        <tr>
-                            <td>DIL(igence) component</td>
-                            <td>{{$student->hw}} + {{$student->bs}} + {{$student->ks}} + {{$student->ac}} = {{$student->dil}}</td>
-                        </tr>
-                        <tr>
-                            <td>Sum</td>
-                            <td>SPE + DIL = {{$student->spe}} + {{$student->dil}} = {{$student->sum}}</td>
-                        </tr>
-                    </table>
-                </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6"><canvas id="radarChart" width="550", height="550"></canvas></div>
+            <div class="col-md-6 vcenter" style="height: 500px;">
+                <table class="table">
+                    <tr>
+                        <td><img src="/img/uploads/{{$student->id}}.png" width="200" height="200"/> </td>
+                    </tr>
+                    <tr>
+                        <td>Kattis account</td>
+                        <td>{{$student->nickname}}</td>
+                    </tr>
+                    <tr>
+                        <td>SPE(ed) component</td>
+                        <td>{{$student->mc}} + {{$student->tc}} = {{$student->spe}}</td>
+                    </tr>
+                    <tr>
+                        <td>DIL(igence) component</td>
+                        <td>{{$student->hw}} + {{$student->bs}} + {{$student->ks}} + {{$student->ac}} = {{$student->dil}}</td>
+                    </tr>
+                    <tr>
+                        <td>Sum</td>
+                        <td>SPE + DIL = {{$student->spe}} + {{$student->dil}} = {{$student->sum}}</td>
+                    </tr>
+                </table>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <p>&nbsp;</p>
+<p>&nbsp;</p>
 
-    <section id="detailed-scores">
-        <h2>Detailed Scores</h2>
-        <table id="ranktable" class="table table-striped table-responsive" width="100%" cellspacing="0">
-            <thead>
+<section id="detailed-scores">
+    <h2>Detailed Scores</h2>
+    <table id="ranktable" class="table table-striped table-responsive" width="100%" cellspacing="0">
+        <thead>
             <tr>
                 <th>Component</th>
                 <th>Sum</th>
@@ -85,8 +87,8 @@
                 <th>11</th>
                 <th>12</th>
             </tr>
-            </thead>
-            <tbody>
+        </thead>
+        <tbody>
             <tr>
                 <td>Mini Contests</td>
                 <td>4</td>
@@ -183,56 +185,56 @@
                 <td></td>
                 <td></td>
             </tr>
-            </tbody>
-        </table>
-    </section>
+        </tbody>
+    </table>
+</section>
 
-    <p>&nbsp;</p>
+<p>&nbsp;</p>
 
-    <section id="achievements">
-        <h2>Achivements</h2>
-        <ol>
-            <li>Let it begins</li>
-            <li>Quick starter</li>
-            <li>Active in class 2/3</li>
-        </ol>
+<section id="achievements">
+    <h2>Achivements</h2>
+    <ol>
+        <li>Let it begins</li>
+        <li>Quick starter</li>
+        <li>Active in class 2/3</li>
+    </ol>
 
-    </section>
+</section>
 
-    <p>&nbsp;</p>
+<p>&nbsp;</p>
 
-    <section id="comments">
-        <h2>Public Comments</h2>
+<section id="comments">
+    <h2>Public Comments</h2>
 
-        <div class="bs-callout bs-callout-success">
-            <h4>IOI Silver Medalist 2015</h4>
-            Own like a pro!
-        </div>
+    <div class="bs-callout bs-callout-success">
+        <h4>IOI Silver Medalist 2015</h4>
+        Own like a pro!
+    </div>
 
-        <div class="bs-callout bs-callout-success">
-            <h4>ICPC Jakarta 2016 runner-up (TeamTam)</h4>
-            Own like a pro!
-        </div>
+    <div class="bs-callout bs-callout-success">
+        <h4>ICPC Jakarta 2016 runner-up (TeamTam)</h4>
+        Own like a pro!
+    </div>
 
-        <div class="bs-callout bs-callout-info">
-            <h4>Active in Class</h4>
-            <ul>
-                <li>Answer 1Q in L1</li>
-                <li>Answer 1Q on Week02</li>
-            </ul>
-        </div>
+    <div class="bs-callout bs-callout-info">
+        <h4>Active in Class</h4>
+        <ul>
+            <li>Answer 1Q in L1</li>
+            <li>Answer 1Q on Week02</li>
+        </ul>
+    </div>
 
-        <div class="bs-callout bs-callout-danger">
-            <h4>Late with HW1 once!</h4>
-            Tsk Tsk
-        </div>
+    <div class="bs-callout bs-callout-danger">
+        <h4>Late with HW1 once!</h4>
+        Tsk Tsk
+    </div>
 
-    </section>
+</section>
 
-    <section id="delete">
-        {!! Form::model($student, ['action' => ['StudentController@destroy', $student->id], 'method' => 'delete', 'id'=>'form-delete']) !!}
-        {!! Form::submit('Delete', ['class'=>'btn btn-danger', 'id'=>'btn-delete']) !!}
-        {!! Form::close() !!}
-    </section>
+<section id="delete">
+    {!! Form::model($student, ['action' => ['StudentController@destroy', $student->id], 'method' => 'delete', 'id'=>'form-delete']) !!}
+    {!! Form::submit('Delete', ['class'=>'btn btn-danger', 'id'=>'btn-delete']) !!}
+    {!! Form::close() !!}
+</section>
 
 @endsection
