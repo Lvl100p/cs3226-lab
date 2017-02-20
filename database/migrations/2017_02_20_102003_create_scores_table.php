961675a10e04fe13a038c2235793b735463c79e7
Kai Lin <kailin.t@gmail.com>
2017-02-20 19:15:28 +0800

    Create Scores table migration and seeder files

---
+++ database/migrations/2017_02_20_102003_create_scores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('scoretype');
+            $table->integer('week');
+            $table->decimal('score', 2, 1);
+            $table->timestamps();
+            $table->primary(['id', 'scoretype', 'week']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
