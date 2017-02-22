a538475b710d221a9c5a423dcf6855aeb08fffa1
Hubert <Lvl100p@gmail.com>
2017-02-23 02:12:03 +0800

    Added migrations for "earned" and "achievements" tables

---
+++ database/migrations/2017_02_22_165055_create_achievement_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAchievementTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->increments('id');
+						$table->string('name');
+						$table->string('description');
+						$table->unsignedTinyInteger('max_tier'); // should be positive
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('achievements');
+    }
+}
