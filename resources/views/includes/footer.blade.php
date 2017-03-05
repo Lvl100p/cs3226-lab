42655239f343319c96b7ae4db09008d1be00ea93
Anna He <jxh604@case.edu>
2017-03-05 16:32:50 +0800

    added like/share to footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,13 @@
 <!-- footer -->
 <footer class="center">
-    Copyright @ 2017 kfwong
+ Copyright @ 2017 kfwong
+<br>
+<div class="fb-like" 
+  data-href="188.166.214.134" 
+  data-layout="button" 
+  data-action="like" 
+  data-size="small" 
+  data-show-faces="true" 
+  data-share="true"></div>
+  
 </footer>
