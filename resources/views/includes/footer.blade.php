73d085086d9c45435fdbc3d55757a2f0dc4fefff
Anna He <jxh604@case.edu>
2017-03-05 16:37:10 +0800

    shows count of likes

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,13 +1,14 @@
 <!-- footer -->
 <footer class="center">
  Copyright @ 2017 kfwong
+ 
 <br>
-<div class="fb-like" 
-  data-href="188.166.214.134" 
-  data-layout="button" 
-  data-action="like" 
-  data-size="small" 
-  data-show-faces="true" 
-  data-share="true"></div>
-  
+
+  <div class="fb-like" 
+    data-href="188.166.214.134" 
+    data-layout="button_count" 
+    data-action="like" 
+    data-size="small" 
+    data-show-faces="true" 
+    data-share="true"></div>
 </footer>
