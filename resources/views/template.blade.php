e969f8ccd2cf94c6f3af14e8f3547619220452ff
kfwong <kfwong@zoho.com>
2017-03-06 07:43:59 +0800

    removed unused code.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -58,15 +58,6 @@
 <!-- child specific scripts -->
 @yield('script')
 <script>
-    window.fbAsyncInit = function () {
-        FB.init({
-            appId: 'YOUR_APP_ID', // App ID
-            channelUrl: '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
-            status: true, // check login status
-            cookie: true, // enable cookies to allow the server to access the session
-            xfbml: true  // parse XFBML
-        });
-    };
 
     // Load the SDK Asynchronously
     (function (d) {
